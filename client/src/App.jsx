89d012109a4c291b10605c762765b8532da7c2f2
alexeyosipov88 <aoosipov88@gmail.com>
2021-11-23 22:13:34 -0500

    add hamberger navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,31 +8,34 @@ import TreatiesForCountry from "./components/countries/TreatiesForCountry";
 function App() {
   return (
     <div>
-      <header>
-        <a href="/">
-        <img src={logo}/>
-        </a>
-      
+      <header className="header">
+        <nav
+         className="flex flex-jc-sb"
+        >
+          <a href="/" className="header__logo">
+            <img style={{ width: "50px" }} src={logo} alt="logo" />
+          </a>
+          <div>
+            <Link to="countries">Countries</Link>
+          </div>
+          <div>
+            <Link to="treaties">Treaties</Link>
+          </div>
+          <div>
+            <Link to="topics">Topics</Link>
+          </div>
+          <div>
+            <Link to="organizations">Organizations</Link>
+          </div>
+          <a href="#" className="header__menu">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+        </nav>
       </header>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <div>
-          <Link to="countries">Countries</Link>
-        </div>
-        <div>
-          <Link to="treaties">Treaties</Link>
-        </div>
-        <div>
-          <Link to="topics">Topics</Link>
-        </div>
-        <div>
-          <Link to="organizations">Organizations</Link>
-        </div>
-      </nav>
+
       <Outlet />
     </div>
   );
