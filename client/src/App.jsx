ab02f282eb71863eeafc343f3bde5c384e8b4048
alexeyosipov88 <aoosipov88@gmail.com>
2021-11-23 21:45:20 -0500

    install sass, add image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "./images/logo.jpg";
+import "./styles/App.scss";
+
+// import "./App.css";
 import CountriesList from "./components/countries/CountriesList";
 import { Outlet, Link } from "react-router-dom";
 import TreatiesForCountry from "./components/countries/TreatiesForCountry";
 function App() {
   return (
     <div>
+      <header>
+        <a href="/">
+        <img src={logo}/>
+        </a>
+      
+      </header>
       <nav
         style={{
           borderBottom: "solid 1px",
@@ -19,10 +27,10 @@ function App() {
           <Link to="treaties">Treaties</Link>
         </div>
         <div>
-        <Link to="topics">Topics</Link>
+          <Link to="topics">Topics</Link>
         </div>
         <div>
-        <Link to="organizations">Organizations</Link>
+          <Link to="organizations">Organizations</Link>
         </div>
       </nav>
       <Outlet />
