2c6a7dd5c1292c11bc52eeea20cd8a0fc914a8e7
alexeyosipov88 <aoosipov88@gmail.com>
2021-12-12 03:57:17 -0500

    add endpoint for counting topics

--- server/index.js
+++ server/index.js
@@ -1,87 +1,104 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-const pool = require("./db")
+const pool = require("./db");
 // middleware
 app.use(cors());
 app.use(express.json()); // req.bodu
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
 app.get("/countries/:id", async (req, res) => {
   try {
-    const treatiesByCountry = await pool.query(`SELECT countries.name AS country_name, treaties .* FROM countries INNER JOIN participation ON countries.id = participation.country_id INNER JOIN treaties ON participation.treaty_id = treaties.id WHERE countries.id = ${req.params.id}`);
-    
-    res.json(treatiesByCountry.rows)
+    const treatiesByCountry = await pool.query(
+      `SELECT countries.name AS country_name, treaties .* FROM countries INNER JOIN participation ON countries.id = participation.country_id INNER JOIN treaties ON participation.treaty_id = treaties.id WHERE countries.id = ${req.params.id}`
+    );
+
+    res.json(treatiesByCountry.rows);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
-})
+});
 
 app.get("/countries", async (req, res) => {
   try {
     const countriesAll = await pool.query("select * from countries");
-    res.json(countriesAll.rows)
+    res.json(countriesAll.rows);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
-})
+});
 
 app.get("/topics/:id", async (req, res) => {
   try {
-    const treatiesByTopic = await pool.query(`SELECT topics.name AS topic_name, treaties.* FROM treaties INNER JOIN treaties_by_topics ON treaties.id = treaties_by_topics.treaty_id INNER JOIN topics ON treaties_by_topics.topic_id = topics.id WHERE topics.id = ${req.params.id}`);
-    res.json(treatiesByTopic.rows)
+    const treatiesByTopic = await pool.query(
+      `SELECT topics.name AS topic_name, treaties.* FROM treaties INNER JOIN treaties_by_topics ON treaties.id = treaties_by_topics.treaty_id INNER JOIN topics ON treaties_by_topics.topic_id = topics.id WHERE topics.id = ${req.params.id}`
+    );
+    res.json(treatiesByTopic.rows);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
-})
+});
 
 app.get("/treaties/", async (req, res) => {
   try {
     const treatiesAll = await pool.query(`SELECT * FROM treaties`);
-    res.json(treatiesAll.rows)
+    res.json(treatiesAll.rows);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
-})
+});
 
 app.get("/topics", async (req, res) => {
   try {
     const topicsAll = await pool.query("SELECT * FROM topics");
-    res.json(topicsAll.rows)
+    res.json(topicsAll.rows);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
-})
+});
 
 app.get("/participation/:id", async (req, res) => {
   try {
-    const participationByTreaty = await pool.query(`SELECT treaties.name AS treaty_name, treaties.city AS city, treaties.concluded AS concluded, treaties.entered_into_force AS treaty_entry_into_force, treaties.status AS treaty_status, countries.name AS country_name, participation.* FROM participation INNER JOIN treaties ON participation.treaty_id = treaties.id INNER JOIN countries ON participation.country_id = countries.id WHERE treaties.id = ${req.params.id}`);
-    res.json(participationByTreaty.rows)
+    const participationByTreaty = await pool.query(
+      `SELECT treaties.name AS treaty_name, treaties.city AS city, treaties.concluded AS concluded, treaties.entered_into_force AS treaty_entry_into_force, treaties.status AS treaty_status, countries.name AS country_name, participation.* FROM participation INNER JOIN treaties ON participation.treaty_id = treaties.id INNER JOIN countries ON participation.country_id = countries.id WHERE treaties.id = ${req.params.id}`
+    );
+    res.json(participationByTreaty.rows);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
-})
+});
 app.get("/count-treaties", async (req, res) => {
   try {
-    const countTreatiesForCountry = await pool.query(`select countries.id, count(treaties) from countries inner join participation ON countries.id = participation.country_id inner join treaties ON treaties.id = participation.treaty_id group by countries.id`);
-    res.json(countTreatiesForCountry.rows)
+    const countTreatiesForCountry = await pool.query(
+      `select countries.id, count(treaties) from countries inner join participation ON countries.id = participation.country_id inner join treaties ON treaties.id = participation.treaty_id group by countries.id`
+    );
+    res.json(countTreatiesForCountry.rows);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
-})
-
+});
+app.get("/count-topics", async (req, res) => {
+  try {
+    const countTreatiesForTopic = await pool.query(
+      `select topics.id, count(treaties) from topics inner join treaties_by_topics ON topics.id = treaties_by_topics.topic_id inner join treaties ON treaties.id = treaties_by_topics.treaty_id group by topics.id`
+    );
+    res.json(countTreatiesForTopic.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.get("/participation", async (req, res) => {
   try {
     const participationAll = await pool.query("SELECT * FROM participation");
-    res.json(participationAll.rows)
+    res.json(participationAll.rows);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
-})
+});
 
- app.listen(4000, () => {
-   console.log("Server has started on port 4000")
- }); 
+app.listen(4000, () => {
+  console.log("Server has started on port 4000");
+});
