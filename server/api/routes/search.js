718350f9518aad786be598e20a67670fd64c84d9
alexeyosipov88 <aoosipov88@gmail.com>
2022-01-17 17:45:15 -0500

    significantly improve search engine after hours of trying ))

--- server/api/routes/search.js
+++ server/api/routes/search.js
@@ -4,32 +4,7 @@ module.exports = (router) => {
   router.get("/search/:keyword", async (req, res) => {
     try {
       const keyword = req.params.keyword.toLowerCase();
-      let result = await search(keyword);
-      // let result = {};
-      // const keywordArr = keyword.split(" ");
-      // console.log(keywordArr);
-      // for (let i = 0; i < keywordArr.length; i++) {
-      //   let tmpResult = await search(keywordArr[i]);
-      //   console.log(tmpResult.topics);
-      //   for (let key in tmpResult) {
-      //     result[key] = [];
-      //     result[key] = result[key].concat(tmpResult[key]);
-      //     console.log("added");
-      //   }
-      // }
-
-      // if (keywordArr.length > 1 && keywordArr.length < 10) {
-      //   console.log(result, 'Resul');
-      //   let tmpResult = [];
-      //   for (let i = 0; i < keywordArr.length; i++) {
-      //     tmpResult = await search(keywordArr[i]);
-      //     for(let key in result) {
-      //       if(tmpResult[key].length > 0) {
-      //         result[key] = result[key].concat(tmpResult[key]);
-      //       }
-      //     }
-      //   }
-      // }
+      let result = await search(keyword); 
       res.json(result);
     } catch (err) {
       console.error(err.message);
--- server/helpers/search.js
+++ server/helpers/search.js
@@ -1,32 +1,152 @@
 const pool = require("../db");
 
+const getUniqueArrayFromSearchResults = (arr) => {
+  let uniqueIds = [];
+  let result = arr.filter((elem) => {
+    if (uniqueIds.includes(elem.id)) {
+      return false;
+    }
+    uniqueIds.push(elem.id);
+    return true;
+  });
+  return result;
+};
+
+const getFromMultipleKeywods = (keyword, callback) => {
+  return new Promise((resolve) => {
+    keyword = keyword.split(/\s/);
+    const promiseForQuery = (keyword, callback) => {
+      return new Promise((resolve) => {
+        resolve(callback(keyword));
+      });
+    };
+    const arrOfPromises = [];
+    for (let i = 0; i < keyword.length; i++) {
+      arrOfPromises.push(promiseForQuery(keyword[i], callback));
+    }
+    Promise.all(arrOfPromises).then((values) => {
+      let finalArrOfResults = [];
+      values = values.map((elem) => {
+        return elem.rows;
+      });
+      for (let i = 0; i < values.length; i++) {
+        if (values.length < 2) {
+          finalArrOfResults.push(values[i]);
+        } else {
+          for (let j = 0; j < values[i].length; j++) {
+            let check = true;
+            for (let k = i + 1; k < values.length; k++) {
+              let idOfResults = values[k].map((elem) => elem.id);
+              if (!idOfResults.includes(values[i][j].id)) {
+                check = false;
+              }
+            }
+            // checking backwards
+            for (let k = i - 1; k >= 0; k--) {
+              let idOfResults = values[k].map((elem) => elem.id);
+              if (!idOfResults.includes(values[i][j].id)) {
+                check = false;
+              }
+            }
+            if (check) finalArrOfResults.push(values[i][j]);
+          }
+        }
+      }
+
+      finalArrOfResults = getUniqueArrayFromSearchResults(finalArrOfResults);
+      resolve(finalArrOfResults);
+    });
+  });
+};
+
 const search = (keyword) => {
   return new Promise(async (resolve, reject) => {
     const result = {};
-    const organizations = await pool.query(
-      `SELECT * FROM organizations WHERE name ILIKE $1 OR name ILIKE $2 OR name ILIKE $3`, [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]);
-    const text = await pool.query(
-      `SELECT english_text.*, treaties.name FROM english_text INNER JOIN treaties ON treaties.id = english_text.treaty_id WHERE text ILIKE $1 OR text ILIKE $2 OR text ILIKE $3`, [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
+    const organizations = await getFromMultipleKeywods(
+      keyword,
+      async (keyword) => {
+        return await pool.query(
+          `SELECT * FROM organizations WHERE name ILIKE $1 OR name ILIKE $2 OR name ILIKE $3`,
+          [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
+        );
+      }
     );
-    const countries = await pool.query(
-      `SELECT * FROM countries WHERE name ILIKE $1 OR name ILIKE $2 OR name ILIKE $3`, [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
-    );
-    const treaties = await pool.query(
-      `SELECT * FROM treaties WHERE name ILIKE $1 OR name ILIKE $2 OR name ILIKE $3`, [`${keyword}%` ,`% ${keyword}%`, `%(${keyword})%`]
-    );
-    const topics = await pool.query(
-      `SELECT * FROM topics WHERE name ILIKE $1 OR name ILIKE $2 OR name ILIKE $3`, [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
-    );
-    const cities = await pool.query(
-      `SELECT * FROM treaties WHERE city ILIKE $1 OR city ILIKE $2 OR city ILIKE $3`, [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
-    );
-    result.text = text.rows;
-    result.organizations = organizations.rows;
-    result.countries = countries.rows;
-    result.topics = topics.rows;
-    result.treaties = treaties.rows;
-    result.cities = cities.rows;
-    resolve(result);
+
+    const text = await getFromMultipleKeywods(keyword, async (keyword) => {
+      return await pool.query(
+        `SELECT english_text.*, treaties.name FROM english_text INNER JOIN treaties ON treaties.id = english_text.treaty_id WHERE text ILIKE $1 OR text ILIKE $2 OR text ILIKE $3`,
+        [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
+      );
+    });
+
+    const countries = await getFromMultipleKeywods(keyword, async (keyword) => {
+      return await pool.query(
+        `SELECT * FROM countries WHERE name ILIKE $1 OR name ILIKE $2 OR name ILIKE $3`,
+        [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
+      );
+    });
+
+    const treaties = await getFromMultipleKeywods(keyword, async (keyword) => {
+      return await pool.query(
+        `SELECT * FROM treaties WHERE name ILIKE $1 OR name ILIKE $2 OR name ILIKE $3`,
+        [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
+      );
+    });
+
+    const topics = await getFromMultipleKeywods(keyword, async (keyword) => {
+      return await pool.query(
+        `SELECT * FROM topics WHERE name ILIKE $1 OR name ILIKE $2 OR name ILIKE $3`,
+        [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
+      );
+    });
+
+    const cities = await getFromMultipleKeywods(keyword, async (keyword) => {
+      return await pool.query(
+        `SELECT * FROM treaties WHERE city ILIKE $1 OR city ILIKE $2 OR city ILIKE $3`,
+        [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
+      );
+    });
+
+    result.text = text;
+    result.organizations = organizations;
+    result.countries = countries;
+    result.topics = topics;
+    result.treaties = treaties;
+    result.cities = cities;
+    console.log(result, "result of search");
+
+    // const organizations = await pool.query(
+    //   `SELECT * FROM organizations WHERE name ILIKE $1 OR name ILIKE $2 OR name ILIKE $3`,
+    //   [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
+    // );
+    // const text = await pool.query(
+    //   `SELECT english_text.*, treaties.name FROM english_text INNER JOIN treaties ON treaties.id = english_text.treaty_id WHERE text ILIKE $1 OR text ILIKE $2 OR text ILIKE $3`,
+    //   [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
+    // );
+    // const countries = await pool.query(
+    //   `SELECT * FROM countries WHERE name ILIKE $1 OR name ILIKE $2 OR name ILIKE $3`,
+    //   [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
+    // );
+    // const treaties = await pool.query(
+    //   `SELECT * FROM treaties WHERE name ILIKE $1 OR name ILIKE $2 OR name ILIKE $3`,
+    //   [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
+    // );
+    // const topics = await pool.query(
+    //   `SELECT * FROM topics WHERE name ILIKE $1 OR name ILIKE $2 OR name ILIKE $3`,
+    //   [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
+    // );
+    // const cities = await pool.query(
+    //   `SELECT * FROM treaties WHERE city ILIKE $1 OR city ILIKE $2 OR city ILIKE $3`,
+    //   [`${keyword}%`, `% ${keyword}%`, `%(${keyword})%`]
+    // );
+
+    // result.text = text.rows;
+    // result.organizations = organizations.rows;
+    // result.countries = countries.rows;
+    // result.topics = topics.rows;
+    // result.treaties = treaties.rows;
+    // result.cities = cities.rows;
+    // resolve(result);
   });
 };
 
