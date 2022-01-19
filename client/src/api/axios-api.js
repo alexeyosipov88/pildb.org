d4b1db0cc44fce236da0aad9015e94e924e2f004
alexeyosipov88 <aoosipov88@gmail.com>
2022-01-19 17:53:42 -0500

    fix api bug

--- client/src/api/axios-api.js
+++ client/src/api/axios-api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 
-const baseURL = process.env.NODE_ENV === "production" ? "api/" :  "http://localhost:4000/api/";
+const baseURL = process.env.NODE_ENV === "production" ? "/" :  "http://localhost:3001/api/";
 
 
 export default axios.create({
