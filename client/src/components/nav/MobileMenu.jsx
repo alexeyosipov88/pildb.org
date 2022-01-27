b7d52b00dad7a8fb9275dd74ea515904db9bd860
alexeyosipov88 <aoosipov88@gmail.com>
2022-01-27 12:06:40 -0500

    change mobile menu links

--- client/src/components/nav/MobileMenu.jsx
+++ client/src/components/nav/MobileMenu.jsx
@@ -1,34 +1,28 @@
 import { Link } from "react-router-dom";
 
-
 const MobileMenu = (props) => {
-  
-
   return (
     <div className={props.overlayMenuClass} onClick={props.clickOnHamburger}>
-       <Link className="link" to="#">
-            About
-          </Link>
-          <Link className="link" to="#">
-            Contact
-          </Link>
-          <Link className="link" to="countries">
-            Countries
-          </Link>
-          <Link className="link" to="treaties">
-            Treaties
-          </Link>
-          <Link className="link" to="topics">
-            Topics
-          </Link>
-          <Link className="link" to="organizations">
-            Organizations
-          </Link>
+      <Link className="link" to="about">
+        About
+      </Link>
+      <Link className="link" to="countries">
+        Countries
+      </Link>
+      <Link className="link" to="treaties">
+        Treaties
+      </Link>
+      <Link className="link" to="topics">
+        Topics
+      </Link>
+      <Link className="link" to="organizations">
+        Organizations
+      </Link>
+      <Link className="link" to="contact">
+        Contact
+      </Link>
     </div>
-  )
-
-
-}
-
+  );
+};
 
 export default MobileMenu;
